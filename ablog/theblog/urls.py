583e9a6699df1ef2fbfa85617e71cbe90361b4b8
estii20 <>
2023-09-22 14:14:35 +0000

    Create post class

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .models import Post
+#from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
 ]
+#
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from dajngo.views.generic import ListView, DetailView
 
 
-def home(request):
-    return render(request, 'home.html', {})
+#def home(request):
+#    return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
